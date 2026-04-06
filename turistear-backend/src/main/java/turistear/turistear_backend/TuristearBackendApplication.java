a84d94648d11c783bfa65ad85cd81abaa8feedcc
Juanse <juansearagon5@gmail.com>
2026-04-06 15:38:11 -0300

    feat: setup inicial backend TuristeAR

---
+++ turistear-backend/src/main/java/turistear/turistear_backend/TuristearBackendApplication.java
@@ -0,0 +1,13 @@
+package turistear.turistear_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristearBackendApplication.class, args);
+	}
+
+}
